5cf2b6e5fd184fc3c5963e39f2aa849d2018ac39
Arun Nair <arunnair@Aruns-MacBook-Pro-2.local>
2019-05-04 09:43:40 -0500

    added styling and html

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,9 +5,20 @@ import Header from "./Layout/Header";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" class="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+            </div>
+          </div>
+        </div>
         <ProjectItem />
       </div>
     );
