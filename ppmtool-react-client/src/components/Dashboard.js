0d8f046d0438a7507c0fe90ca7f50810fbbc250c
Arun Nair <arunnair@Aruns-MacBook-Pro-2.local>
2019-05-04 09:18:28 -0500

    added bootstrap and navbar

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import Header from "./Layout/Header";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <Header />
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
