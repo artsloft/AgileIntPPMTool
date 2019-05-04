23cbaa447927ed8cfb6755111f7dcb4e9b2e2872
Arun Nair <arunnair@Aruns-MacBook-Pro-2.local>
2019-05-03 21:49:38 -0500

    added controller, services, and repository

---
+++ PPMToolFullStack/src/main/java/com/elastitrader/ppmtool/web/ProjectController.java
@@ -0,0 +1,42 @@
+package com.elastitrader.ppmtool.web;
+
+import com.elastitrader.ppmtool.domain.Project;
+import com.elastitrader.ppmtool.services.ProjectService;
+import com.elastitrader.ppmtool.services.ValidationErrorMapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ValidationErrorMapService validationErrorMapService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = validationErrorMapService.CreateValidationErrorMap(result);
+        if (errorMap != null) return errorMap;
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+}
